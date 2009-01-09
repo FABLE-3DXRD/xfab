2d16f83cd4c048c1c3505b5c9e3ee8c5aa31c2da
h_osholm <h_osholm@users.sourceforge.net>
2009-01-09 22:59:27 +0000

    bugfixed: missed a 2pi in the calculation of the ubi. Well is does not really matter as the should be very close to zero anyway

--- test/test_tools.py
+++ test/test_tools.py
@@ -47,12 +47,13 @@ class test_rodrigues(unittest.TestCase):
         phi1 = 0.13
         PHI  = 0.4
         phi2 = 0.21
-        cell = [3,4,5,90,90,90]
+        cell = [3,4,5,80,95,100]
         Umat = tools.euler2U(phi1,PHI,phi2)
-        ubi = n.linalg.inv(n.dot(Umat, tools.FormB(cell)))#*2*n.pi
+        ubi = n.linalg.inv(n.dot(Umat, tools.FormB(cell)))*2*n.pi
         rodubi = tools.ubi2rod(ubi)
         rodU = tools.U2rod(Umat)
         diff = n.abs(rodubi-rodU).sum()
+        print diff
         self.assertAlmostEquals(diff,0,9)
 
 
