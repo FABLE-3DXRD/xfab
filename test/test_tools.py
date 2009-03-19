acab190e1bad2eb58701f5c73a61a03c3140d173
jetteodder <jetteodder@users.sourceforge.net>
2009-03-19 09:52:26 +0000

    Removed obsolete tests on Omega matrix using quarternion convention from test/test_tools.py

--- test/test_tools.py
+++ test/test_tools.py
@@ -90,69 +90,6 @@ class test_twotheta(unittest.TestCase):
         diff = n.abs(tth-tth2)
         self.assertAlmostEquals(diff,0,9)
         
-class test_quarternions(unittest.TestCase):
-
-    def test_quart2Omega(self):
-        omega = n.random.rand()*360.
-        Omega = tools.form_omega_mat(omega*n.pi/180.)
-        Omega_quart = tools.quart_to_omega(omega,0,0)
-        diff = n.abs(Omega-Omega_quart).sum()
-        self.assertAlmostEquals(diff,0,9)
-        
-    def test_find_omega(self):
-        # generate random gvector
-        hkl = n.array([round(n.random.rand()*10-5),round(n.random.rand()*10-5),round(n.random.rand()*10-5)])
-        ucell = n.array([3.5+n.random.rand(),3.5+n.random.rand(),3.5+n.random.rand(),89.5+n.random.rand(),89.5+n.random.rand(),89.5+n.random.rand()])
-        B = tools.form_b_mat(ucell)
-        U = tools.euler_to_u(n.random.rand()*2.*n.pi,n.random.rand()*2.*n.pi,n.random.rand()*n.pi)
-        wavelength = 0.95 + n.random.rand()*0.1
-        gvec = n.dot(U,n.dot(B,hkl))
-        tth = tools.tth(ucell, hkl, wavelength)        
-        # calculate corresponding eta and Omega using tools.find_omega_quart
-        (omega1, eta1) = tools.find_omega_quart(gvec*wavelength/(4.*n.pi),tth,0,0)
-        Om1 = []
-        for i in range(len(omega1)):
-            Om1.append(tools.quart_to_omega(omega1[i]*180./n.pi,0,0))  
-        # calculate corresponding eta and Omega using tools.find_omega
-        omega2 = tools.find_omega(gvec,tth)
-        Om2 = []
-        for i in range(len(omega2)):
-            Om2.append(tools.form_omega_mat(omega2[i]))
-        # assert  
-        for i in range(len(eta1)):          
-            diff = n.abs(Om1[i]-Om2[i]).sum()
-            self.assertAlmostEquals(diff,0,9)        
-            
-    def test_find_omega_wedge(self):
-        # generate random gvector
-        hkl = n.array([round(n.random.rand()*10-5),round(n.random.rand()*10-5),round(n.random.rand()*10-5)])
-        ucell = n.array([3.5+n.random.rand(),3.5+n.random.rand(),3.5+n.random.rand(),89.5+n.random.rand(),89.5+n.random.rand(),89.5+n.random.rand()])
-        B = tools.form_b_mat(ucell)
-        U = tools.euler_to_u(n.random.rand()*2.*n.pi,n.random.rand()*2.*n.pi,n.random.rand()*n.pi)
-        wavelength = 0.95 + n.random.rand()*0.1
-        gvec = n.dot(U,n.dot(B,hkl))
-        tth = tools.tth(ucell, hkl, wavelength)
-        wedge = n.random.rand()-.5
-        # calculate corresponding eta and Omega using tools.find_omega_quart
-        (omega1, eta1) = tools.find_omega_quart(gvec*wavelength/(4.*n.pi),tth,0,wedge)
-        Om1 = []
-        for i in range(len(omega1)):
-            Om1.append(tools.quart_to_omega(omega1[i]*180./n.pi,0,wedge))  
-        # calculate corresponding eta and Omega using tools.find_omega_wedge
-        Phi_y = n.array([[ n.cos(wedge), 0, n.sin(wedge)],
-                         [0         , 1, 0        ],
-                         [-n.sin(wedge), 0, n.cos(wedge)]])
-        gvec = n.dot(n.transpose(Phi_y),gvec)
-        (omega2, eta2) = tools.find_omega_wedge(gvec,tth,-wedge)
-        Om2 = []
-        for i in range(len(omega2)):
-            Om2.append(n.dot(Phi_y,n.dot(tools.form_omega_mat(omega2[i]),n.transpose(Phi_y))))
-        #assert  
-        for i in range(len(eta1)):          
-            diff = n.abs(eta1[i]-eta2[i])
-            self.assertAlmostEquals(diff,0,9)
-            diff = n.abs(Om1[i]-Om2[i]).sum()
-            self.assertAlmostEquals(diff,0,9)        
 
 class test_general_orientation(unittest.TestCase):
 
@@ -182,53 +119,6 @@ class test_general_orientation(unittest.TestCase):
             diff = n.abs(Om1[i]-Om2[i]).sum()
             self.assertAlmostEquals(diff,0,9)        
 
-    def test_find_omega_general_wedge(self):
-        # generate random gvector
-        hkl = n.array([round(n.random.rand()*10-5),round(n.random.rand()*10-5),round(n.random.rand()*10-5)])
-        ucell = n.array([3.5+n.random.rand(),3.5+n.random.rand(),3.5+n.random.rand(),89.5+n.random.rand(),89.5+n.random.rand(),89.5+n.random.rand()])
-        B = tools.form_b_mat(ucell)
-        U = tools.euler_to_u(n.random.rand()*2.*n.pi,n.random.rand()*2.*n.pi,n.random.rand()*n.pi)
-        wavelength = 0.95 + n.random.rand()*0.1
-        gvec = n.dot(U,n.dot(B,hkl))
-        tth = tools.tth(ucell, hkl, wavelength)
-#        wedge = n.random.rand()-.5
-        wedge = n.random.rand()*10-5
-        Phi_y = n.array([[ n.cos(wedge), 0, n.sin(wedge)],
-                         [0         , 1, 0        ],
-                         [-n.sin(wedge), 0, n.cos(wedge)]])
-        # calculate corresponding eta and Omega using tools.find_omega_general
-        (omega1, eta1) = tools.find_omega_general(gvec*wavelength/(4.*n.pi),tth,0,wedge)
-        Om1 = []
-        for i in range(len(omega1)):
-            Om1.append(tools.form_omega_mat_general(omega1[i],0,wedge))  
-        if len(eta1) == 2: 
-            eta1 = n.array(eta1)
-            Om1 = n.array(Om1)
-            order = n.argsort(eta1)
-            Om1 = Om1[order]
-            eta1 = eta1[order]
-        # calculate corresponding eta and Omega using tools.find_omega_wedge
-        (omega2, eta2) = tools.find_omega_wedge(gvec,tth,-wedge)
-        Om2 = []            
-        for i in range(len(omega2)):
-            Om2.append(n.dot(Phi_y,tools.form_omega_mat(omega2[i])))
-        if len(eta2) == 2: 
-            eta2 = n.array(eta2)
-            Om2 = n.array(Om2)
-            order = n.argsort(eta2)
-            eta2 = eta2[order]
-            Om2 = Om2[order]
-        #assert  
-        for i in range(len(omega1)):    
-            if i==0:
-                print 'test_find_omega_general_wedge'
-#            print eta1[i],eta2[i]
-#            print Om1[i]
-#            print Om2[i]
-            diff = n.abs(eta1[i]-eta2[i])
-            self.assertAlmostEquals(diff,0,9)
-            diff = n.abs(Om1[i]-Om2[i]).sum()
-            self.assertAlmostEquals(diff,0,9)        
 
 
 class test_ABepsilon(unittest.TestCase):
