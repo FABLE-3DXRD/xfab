083f06ee6ddcea50a31cb6e6533c9a79d38ae48a
jetteodder <jetteodder@users.sourceforge.net>
2010-02-08 12:38:41 +0000

    Bugfix of Umis, permutations should be unitary rotations.

--- xfab/symmetry.py
+++ xfab/symmetry.py
@@ -5,21 +5,13 @@ related stuff
 
 from numpy import zeros, arccos, pi, dot, transpose, array, concatenate, cos, sin, dot
 from numpy.linalg import det, inv
+from xfab import tools
 
 
 def Umis(umat_1, umat_2, crystal_system):
-    """
-    
-     Determines misorientations between grain orientations 
-    
-     calc(ubifile,crystal_system)
-    
-     Input:    graininfo.mat is the simul_farfield file containing simulated
-               orientations
-    
-               ubifile: file with orientations as (UB)^{-1} from
-                        GrainSpotter or ImageD11-index
-    
+    """    
+     Determines misorientations between grain orientations     
+     Input:    umat_1 and umat_2 orientation matrices   
                crystal_system  1: Triclinic
                                2: Monoclinic
                                3: Orthorhombic
@@ -27,15 +19,15 @@ def Umis(umat_1, umat_2, crystal_system):
                                5: Trigonal
                                6: Hexagonal
                                7: Cubic
-
+     Output:   (rotation number, misorientation in deg)
     """
 
-    #Permutation matrix defined by crystal system 
-    perm = permutations(crystal_system)
-    nperm = perm.shape[0]
+    #rotation matrix defined by crystal system 
+    rot = rotations(crystal_system)
+    nrot = rot.shape[0]
     t_save = zeros((0, 2))
-    for k in range(nperm):
-        g_vector = dot(umat_2, transpose(dot(umat_1, perm[k])))
+    for k in range(nrot):
+        g_vector = dot(umat_2, transpose(dot(umat_1, rot[k])))
         detg = det(g_vector)
         if detg < 0.9999 or detg > 1.0001:
             print 'mistake %f' % detg
@@ -51,26 +43,41 @@ def Umis(umat_1, umat_2, crystal_system):
     return t_save
 
     
-def add_perm(umat, crystal_system):
+def add_perm(hkl, crystal_system):
     """
     apply the permutation according to the crystal system on the 
-    orientation matrix U and print the result
+    hkl and print the result
     """
+    
     perm = permutations(crystal_system)
     nperm = perm.shape[0]
     
     for k in range(nperm):
-        print dot(umat, perm[k])
+        print dot(perm[k],hkl)
 
+        
+def add_rot(umat, crystal_system):
+    """
+    apply the rotation according to the crystal system on the 
+    orientation matrix U and print the result
+    """
+    
+    rot = rotations(crystal_system)
+    nrot = rot.shape[0]
+    
+    for k in range(nrot):
+        print dot(umat, rot[k])
+
+        
 def permutations(crystal_system):
     
     """ 
     permutations returns the set of indistinguasible lattice permutations
     
     perm = permutations(crystal_system)
+    hkl_new = perm*hkl
     
-    crystal_system can be one of the following values
-    
+    crystal_system can be one of the following values  
     1: Triclinic
     2: Monoclinic
     3: Orthorhombic
@@ -166,3 +173,77 @@ def permutations(crystal_system):
 
     return perm
 
+    
+def rotations(crystal_system):
+    
+    """ 
+    rotations returns the set of unitary rotation matrices
+    corresponding to the indistinguasible lattice permutations
+    The values of the function only differ from permutations for 
+    trigonal and hexagonal crystal systems
+    
+    rot = rotations(crystal_system)
+    U_new = U*rot
+    
+    U_new*B*perm*hkl = U*B*hkl, so U_new = U*B*perminv*Binv and
+    rot = B*perminv*Binv. If B or perm is diagonal, rot = perminv.
+    perminv included in perm, but to get the i'th entry of rot
+    to correspond to the i'th entry of perm one must use perminv.
+    
+    crystal_system can be one of the following values   
+    1: Triclinic
+    2: Monoclinic
+    3: Orthorhombic
+    4: Tetragonal
+    5: Trigonal
+    6: Hexagonal
+    7: Cubic
+    
+    Jette Oddershede, Riso, 8/2/2010
+    """
+
+    if crystal_system < 1 or crystal_system > 7:
+        raise ValueError('Crystal system shoud have a value between 1 and 7')
+
+    if crystal_system == 1: # Triclinic
+        rot = permutations(crystal_system)
+        for i in range(len(rot)):       
+            rot[i] = inv(rot[i])
+
+    if crystal_system == 2: # Monoclinic
+        rot = permutations(crystal_system)
+        for i in range(len(rot)):       
+            rot[i] = inv(rot[i])
+
+    if crystal_system == 3: # Orthorhombic
+        rot = permutations(crystal_system)
+        for i in range(len(rot)):       
+            rot[i] = inv(rot[i])
+ 
+    if crystal_system == 4: # Tetragonal
+        rot = permutations(crystal_system)
+        for i in range(len(rot)):       
+            rot[i] = inv(rot[i])
+
+    if crystal_system == 5: # Trigonal
+        perm = permutations(crystal_system)
+        B = tools.form_b_mat([1.,1.,1.,90.,90.,120.])
+        Binv = inv(B)
+        rot = zeros((6, 3, 3))
+        for i in range(len(perm)):       
+            rot[i] = dot(B,dot(inv(perm[i]),Binv))
+                
+    if crystal_system == 6: # Hexagonal
+        perm = permutations(crystal_system)
+        B = tools.form_b_mat([1.,1.,1.,90.,90.,120.])
+        Binv = inv(B)
+        rot = zeros((12, 3, 3))
+        for i in range(len(perm)):       
+            rot[i] = dot(B,dot(inv(perm[i]),Binv))
+
+    if crystal_system == 7: # Cubic
+        rot = permutations(crystal_system)
+        for i in range(len(rot)):       
+            rot[i] = inv(rot[i])
+
+    return rot
