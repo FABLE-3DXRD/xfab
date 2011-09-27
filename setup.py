286121cc8c26363ce0198310e08f338ec39b7840
jetteodder <jetteodder@users.sourceforge.net>
2011-09-27 08:37:12 +0000

    added gff_to_ubi and ubi_to_gff scripts

--- setup.py
+++ setup.py
@@ -12,5 +12,6 @@ setup(
   download_url='http://sourceforge.net/project/showfiles.php?group_id=82044&package_id=309377',
   url='http://fable.wiki.sourceforge.net/xfab',
   packages=['xfab'],
-  package_dir={"xfab": "xfab"}
+  package_dir={"xfab": "xfab"},
+  scripts=["scripts/gff_to_ubi.py","scripts/ubi_to_gff.py"]
 )
