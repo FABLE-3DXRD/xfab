f819ef8cc8af25fc6f194c8e202a8643727f21b5
jetteodder <jetteodder@users.sourceforge.net>
2013-06-04 09:03:23 +0000

    added make_gve to xfab

---
+++ scripts/make_gve.py
@@ -0,0 +1,21 @@
+# Create objects to manipulate - they hold your data
+#
+import sys
+from ImageD11 import peakmerge, indexing, transformer
+mypeakmerger = peakmerge.peakmerger()
+mytransformer = transformer.transformer()
+myindexer = indexing.indexer()
+#
+# Your work starts here:
+#
+filename = sys.argv[1]
+outfile =  sys.argv[2]
+parfile =  sys.argv[3]
+mytransformer.loadfiltered(  filename )
+mytransformer.loadfileparameters( parfile )
+mytransformer.compute_tth_eta( )
+mytransformer.getcolumn(  'tth' )
+mytransformer.getcolumn(  'eta' )
+mytransformer.addcellpeaks( )
+mytransformer.computegv( )
+mytransformer.savegv(  outfile )
