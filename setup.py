001d6ca9e499f33ea3073d3e0c20d19c39ab4fcd
jetteodder <jetteodder@users.sourceforge.net>
2015-04-13 12:02:27 +0000

    updated environment settings and contact info

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   version='0.0.2',
   description='Crystallographic toolbox and library',
   license='GPL', maintainer='Henning Osholm Soerensen and Jon Wright',
-  maintainer_email='henning.sorensen@risoe.dk or wright@esrf.eu',
+  maintainer_email='osholm@nano.ku.dk or wright@esrf.eu',
   download_url='http://sourceforge.net/project/showfiles.php?group_id=82044&package_id=309377',
   url='http://fable.wiki.sourceforge.net/xfab',
   packages=['xfab'],
