929f671286c57aeff17b145f40697667e56b46e7
Jonathan Wright <wright@esrf.fr>
2018-12-14 15:23:05 +0100

    release on pypi (for CI of dependencies)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   version='0.0.3',
   description='Crystallographic toolbox and library',
   license='GPL', maintainer='Henning Osholm Soerensen and Jon Wright',
-  maintainer_email='osholm@nano.ku.dk or wright@esrf.eu',
+  maintainer_email='osholm@nano.ku.dk, wright@esrf.eu',
   download_url='http://sourceforge.net/project/showfiles.php?group_id=82044&package_id=309377',
   url='http://fable.wiki.sourceforge.net/xfab',
   packages=['xfab'],
