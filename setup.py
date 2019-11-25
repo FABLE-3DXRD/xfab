3a7d95c04ce809c68cd2ee1696bb9837732fbf4a
Jonathan Wright <wright@esrf.fr>
2019-11-25 11:18:18 +0100

    v0.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 
 setup(
   name='xfab',
-  version='0.0.3',
+  version='0.0.4',
   description='Crystallographic toolbox and library',
   license='GPL', maintainer='Henning Osholm Soerensen and Jon Wright',
   maintainer_email='osholm@nano.ku.dk, wright@esrf.eu',
